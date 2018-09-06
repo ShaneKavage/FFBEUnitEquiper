781901a9a98c858434e4c5794b8b72e55d666ff3
ShaneKavage <42551406+ShaneKavage@users.noreply.github.com>
2018-09-05 20:06:40 -0500

    Added mainscreen
    basic main screen, needs much work to display the database

---
+++ Unit Equipper Main Files/MainScreen.java
@@ -0,0 +1,28 @@
+package com.example.shane.finalapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+        ListView view;
+
+        view =  findViewById(R.id.listView);
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        List<String> quotes = databaseAccess.getQuotes();
+        databaseAccess.close();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, quotes);
+        view.setAdapter(adapter);
+    }
+}
